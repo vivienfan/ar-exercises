13444884bf4c5ecc26e8cb009e889e785458bfe3
vagrant <vivien.fan.1224@gmail.com>
2017-06-06 16:30:46 +0000

    deleting a tuple from stores table

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,3 +7,5 @@ puts "----------"
 store0 = Store.create name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true
 store1 = Store.create name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true
 store2 = Store.create name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false
+
+puts "Now we have #{Store.count} stores"
